e995982753837d5079db49ef1503b5f5e1adc2c2
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-02-20 16:08:09 +0900

    [Add] first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyB74aMeF-jS8Lz40TvMuX2ukcAYO7yw5uM",
+    authDomain: "nuxtapp-fd0e0.firebaseapp.com",
+    databaseURL: "https://nuxtapp-fd0e0.firebaseio.com",
+    projectId: "nuxtapp-fd0e0",
+    storageBucket: "nuxtapp-fd0e0.appspot.com",
+    messagingSenderId: "757540371578"
+  })
+}
+
+export default firebase
