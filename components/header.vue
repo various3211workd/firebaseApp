13bd9d9510f01623fa134246a2ff9295e250ef04
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-03-05 16:59:13 +0900

    [change] header layout

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="header">
-    <h1>Nuxt.js Sample!</h1>
-    <ul class="menu">
-      <li>メニュー</li>
-    </ul>
-  </div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <p style="font-size: 30px; font-family: Impact, Charcoal;">SAMPLE</p>
+      </a>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link" href="account">
+            Account
+          </a>
+          <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="account">
+              Login
+            </a>
+            <a class="navbar-item" href="">
+              test
+            </a>
+          </div>
+        </div>
+        <a class="navbar-item" href="https://github.com/various3211workd/firebaseApp" target="_blank">
+          GitHub
+        </a>
+      </div>
+    </div>
+  </nav>
+
 </template>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
         firebase
       </h1>
