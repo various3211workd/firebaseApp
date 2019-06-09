de1641d137f8177ee1255aa6731f1891ea121341
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-06-09 12:16:03 +0900

    [Add] .env

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyB74aMeF-jS8Lz40TvMuX2ukcAYO7yw5uM",
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: "nuxtapp-fd0e0.firebaseapp.com",
     databaseURL: "https://nuxtapp-fd0e0.firebaseio.com",
     projectId: "nuxtapp-fd0e0",
     storageBucket: "nuxtapp-fd0e0.appspot.com",
-    messagingSenderId: "757540371578"
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
   })
 }
 
